7220fd803f25c5017921ecc9268c2353aa5cba12
Tahir Asadov <tahir-asadov@outlook.com>
2022-01-23 06:37:52 +0400

    Remove layout from service is unavailable page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,15 +1,1 @@
-@extends('layouts.public')
-
-@section('body_class', 'error')
-
-@section('title', __('Service Unavailable'))
-
-@section('content')
-  <section class="error">
-    <div class="row">
-      <div class="container">
-        <h1><i class="fas fa-exclamation-triangle"></i>{{__('Service Unavailable')}}</h1>
-      </div><!-- .container -->  
-    </div><!-- .row -->  
-  </section><!-- .error -->
-@endsection
+Service Unavailable
